330ceb887b3e7623678d515d151c5fcc7582545e
deewakar <dakshftw01@gmail.com>
2024-11-23 11:55:51 +0530

    config

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,82 @@
+return {
+	{
+		-- obsidian
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		ft = "markdown",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "Work",
+					path = "~/Documents/notes/top secrets/",
+				},
+			},
+			ui = {
+				enable = false,
+				checkboxes = {
+					[" "] = { char = "☐", hl_group = "ObsidianTodo" },
+					["x"] = { char = "", hl_group = "ObsidianDone" },
+					[">"] = { char = "", hl_group = "ObsidianRightArrow" },
+				},
+			},
+		},
+	},
+	{
+		-- pretty markdown
+		"MeanderingProgrammer/render-markdown.nvim",
+		-- enabled = false,
+		opts = {
+			heading = {
+				width = "block",
+				min_width = 50,
+				border = true,
+				backgrounds = {
+					"RenderMarkdownH1Bg",
+					"RenderMarkdownH2Bg",
+					"RenderMarkdownH3Bg",
+					"RenderMarkdownH4Bg",
+					"RenderMarkdownH5Bg",
+					"RenderMarkdownH6Bg",
+				},
+				foregrounds = {
+					"RenderMarkdownH1",
+					"RenderMarkdownH2",
+					"RenderMarkdownH3",
+					"RenderMarkdownH4",
+					"RenderMarkdownH5",
+					"RenderMarkdownH6",
+				},
+			},
+			render_modes = { "n", "v", "i", "c" },
+			checkbox = {
+
+				unchecked = { icon = "󰄱 " },
+				checked = { icon = " " },
+				custom = { todo = { raw = "[>]", rendered = "󰥔 " } },
+			},
+			code = {
+				position = "right",
+				width = "block",
+				left_pad = 2,
+				right_pad = 4,
+			},
+		},
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+	},
+
+	{
+		-- markdown preview
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		keys = {},
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+}
---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,157 @@
+return {
+	--	{
+	--		"vague2k/vague.nvim",
+	--		config = function()
+	--			require("vague").setup({
+	--				-- optional configuration here
+	--				transparent = false,
+	--				style = {
+	--					headings = "bold",
+	--				},
+	--				colors = {
+	--					bg = "#000000",
+	--					func = "#bc96b0",
+	--				},
+	--				--		colors = {
+	--				--				,
+	--				--				keyword = "#787bab",
+	--				--				line = "#282830",
+	--				--				-- string = "#d4bd98",
+	--				--				string = "#8a739a",
+	--				--				-- string = "#f2e6ff",
+	--				--				-- number = "#f2e6ff",
+	--				--				-- string = "#d8d5b1",
+	--				--				number = "#8f729e",
+	--				--				-- type = "#dcaed7",
+	--				--			},
+	--			})
+	--
+	--			vim.cmd.colorscheme("vague")
+	--		end,
+	--	},
+	--	{
+	--		"catppuccin/nvim",
+	--		name = "catppuccin",
+	--		priority = 1000,
+	--		config = function()
+	--			require("catppuccin").setup({
+	--				background = {
+	--					light = "latte",
+	--					dark = "mocha",
+	--				},
+	--				no_italic = true,
+	--				transparent_background = true,
+	--				color_overrides = {
+	--					mocha = {},
+	--				},
+	--			})
+	--			vim.cmd.colorscheme("catppuccin-mocha")
+	--		end,
+	--	},
+	--	{
+	--		"mellow-theme/mellow.nvim",
+	--		config = function()
+	--			vim.g.mellow_italic_functions = false
+	--			vim.g.mellow_bold_functions = true
+	--			--		vim.g.mellow_transparent = true
+	--			--		vim.g.mellow_highlight_overrides = {
+	--			--			["NormalFloat"] = { bg = "None" }, -- Normal text in floating windows.
+	--			--			["FloatBorder"] = { bg = "None" }, -- Border of floating windows.
+	--
+	--			--			["TelescopeNormal"] = { bg = "None" },
+	--			--			["TelescopeBorder"] = { bg = "None" },
+	--			--			["TelescopePreviewTitle"] = { bg = "None" },
+	--			--			["TelescopeResultsTitle"] = { bg = "None" },
+	--			--			["TelescopePromptTitle"] = { bg = "None" },
+	--			--			["TelescopePromptNormal"] = { bg = "None" },
+	--			--			["TelescopePromptBorder"] = { bg = "None" },
+	--
+	--			--			["Visual"] = { bg = "#131314" }, -- Visual mode selection
+	--			--		}
+	--			vim.cmd([[colorscheme mellow]])
+	--		end,
+	--	},
+	--	{
+	--		"dgox16/oldworld.nvim",
+	--		lazy = false,
+	--		priority = 1000,
+	--		config = function()
+	--			require("oldworld").setup({
+	--				integrations = {
+	--					telescope = false,
+	--				},
+	--			})
+	--			vim.cmd.colorscheme("oldworld")
+	--		end,
+	--	},
+	--	{
+	--		"sam4llis/nvim-tundra",
+	--		lazy = false,
+	--		priority = 1000,
+	--		config = function()
+	--			require("nvim-tundra").setup({
+	--				transparent_background = false,
+	--				dim_inactive_windows = {
+	--					enabled = false,
+	--					color = nil,
+	--				},
+	--				sidebars = {
+	--					enabled = true,
+	--					color = nil,
+	--				},
+	--				editor = {
+	--					search = {},
+	--					substitute = {},
+	--				},
+	--				syntax = {
+	--					booleans = { bold = true, italic = true },
+	--					comments = { bold = true, italic = true },
+	--					conditionals = {},
+	--					constants = { bold = true },
+	--					fields = {},
+	--					functions = {},
+	--					keywords = {},
+	--					loops = {},
+	--					numbers = { bold = true },
+	--					operators = { bold = true },
+	--					punctuation = {},
+	--					strings = {},
+	--					types = { italic = true },
+	--				},
+	--				plugins = {
+	--					lsp = true,
+	--					semantic_tokens = true,
+	--					treesitter = true,
+	--					telescope = true,
+	--					nvimtree = true,
+	--					cmp = true,
+	--					context = true,
+	--					dbui = true,
+	--					gitsigns = true,
+	--					neogit = true,
+	--					textfsm = true,
+	--				},
+	--				overwrite = {
+	--					colors = {},
+	--					highlights = {},
+	--				},
+	--			})
+	--			vim.g.tundra_biome = "arctic"
+	--			vim.opt.background = "dark"
+	--			vim.cmd.colorscheme("tundra")
+	--		end,
+	--	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				background = {
+					dark = "dragon",
+				},
+			})
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+}
