14c086bcdd0b04ead25652d86da389ebb277a58b
deewakar <dakshftw01@gmail.com>
2025-01-30 13:30:41 +0530

    new theme

--- init.lua
+++ init.lua
@@ -631,7 +631,9 @@ require("lazy").setup({
 				},
 				pyright = {},
 				rust_analyzer = {},
-				phpactor = {},
+				phpactor = {
+					cmd = { "phpactor", "language-server" },
+				},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
