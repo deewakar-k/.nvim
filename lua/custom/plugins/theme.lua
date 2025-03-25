99ff232baa982899f7e12860dda11625c5c05464
deewakar-k <dakshftw01@gmail.com>
2025-03-25 11:30:23 +0530

    refactor theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,261 +1,32 @@
 return {
-	--	{
-	--		"vague2k/vague.nvim",
-	--		config = function()
-	--			require("vague").setup({
-	--				-- optional configuration here
-	--				transparent = true,
-	--				style = {
-	--					headings = "bold",
-	--				},
-	--				colors = {
-	--					bg = "#000000",
-	--					func = "#bc96b0",
-	--					keyword = "#787bab",
-	--					line = "#282830",
-	--					-- string = "#d4bd98",
-	--					string = "#8a739a",
-	--					-- string = "#f2e6ff",
-	--					-- number = "#f2e6ff",
-	--					-- string = "#d8d5b1",
-	--					number = "#8f729e",
-	--					-- type = "#dcaed7",
-	--				},
-	--			})
-	--
-	--			vim.cmd.colorscheme("vague")
-	--		end,
-	--	},
-	--	{
-	--		"catppuccin/nvim",
-	--		name = "catppuccin",
-	--		priority = 1000,
-	--		config = function()
-	--			require("catppuccin").setup({
-	--				background = {
-	--					light = "latte",
-	--					dark = "mocha",
-	--				},
-	--				no_italic = true,
-	--				transparent_background = false,
-	--				color_overrides = {
-	--					mocha = {},
-	--				},
-	--			})
-	--			vim.cmd.colorscheme("catppuccin-mocha")
-	--		end,
-	--	},
 	{
-		"dgox16/oldworld.nvim",
+		"tjdevries/colorbuddy.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require("oldworld").setup({
-				integrations = {
-					telescope = false,
-				},
-			})
-			vim.cmd.colorscheme("oldworld")
+			vim.cmd.colorscheme("gruvbuddy")
 		end,
 	},
-	--	{
-	--		"rebelot/kanagawa.nvim",
-	--		lazy = false,
-	--		priority = 1000,
-	--		config = function()
-	--			require("kanagawa").setup({
-	--				background = {
-	--					dark = "dragon",
-	--				},
-	--			})
-	--			vim.cmd.colorscheme("kanagawa")
-	--		end,
-	--	},
-	--	{
-	--		"arcticicestudio/nord-vim",
-	--		config = function()
-	--			vim.cmd([[colorscheme nord]])
-	--		end,
-	--	},
-	--	{
-	--		"navarasu/onedark.nvim",
-	--		config = function()
-	--			require("onedark").setup({
-	--				style = "dark",
-	--			})
-	--			vim.cmd.colorscheme("onedark")
-	--		end,
-	--	},
-	--	{
-	--		"rose-pine/neovim",
-	--		name = "rose-pine",
-	--		config = function()
-	--			require("rose-pine").setup({
-	--				variant = "dawn",
-	--			})
-	--			vim.cmd.colorscheme("rose-pine")
-	--		end,
-	--	},
-	--	{
-	--		"cpwrs/americano.nvim",
-	--		config = function()
-	--			-- Defaults
-	--			require("americano").setup({
-	--				terminal = true, -- Set terminal colors
-	--				overrides = {}, -- Override americano highlight groups
-	--			})
-	--			vim.cmd.colorscheme("americano")
-	--		end,
-	--	},
-	--	{
-	--		"yorumicolors/yorumi.nvim",
-	--		config = function()
-	--			vim.cmd("colorscheme yorumi")
-	--		end,
-	--	},
-	--	{
-	--		"aktersnurra/no-clown-fiesta.nvim",
-	--		priority = 1000,
-	--		lazy = false,
-	--		config = function()
-	--			require("no-clown-fiesta").setup({
-	--				styles = {
-	--					italic = true,
-	--				},
-	--			})
-	--			vim.cmd([[colorscheme no-clown-fiesta]])
-	--		end,
-	--	},
-	--	{
-	--		"mhartington/oceanic-next",
-	--		config = function()
-	--			-- Set the colorscheme
-	--			vim.cmd([[colorscheme OceanicNext]])
-	--
-	--			-- Apply highlight settings using vim.cmd
-	--			vim.cmd([[
-	--						hi Normal guibg=NONE ctermbg=NONE
-	--						hi LineNr guibg=NONE ctermbg=NONE
-	--						hi SignColumn guibg=NONE ctermbg=NONE
-	--						hi EndOfBuffer guibg=NONE ctermbg=NONE
-	--					]])
-	--		end,
-	--	},
-	--	{
-	--		"metalelf0/base16-black-metal-scheme",
-	--		config = function()
-	--			-- Set the colorscheme
-	--			vim.cmd([[colorscheme base16-black-metal-gorgoroth]])
-	--
-	--			-- Apply theme overrides
-	--			local function black_metal_theme_overrides()
-	--				vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#912222" })
-	--				vim.api.nvim_set_hl(0, "TSComment", { fg = "#555555", gui = nil })
-	--				vim.api.nvim_set_hl(0, "Visual", { bg = "#9b8d7f", fg = "#1e1e1e" })
-	--				vim.api.nvim_set_hl(0, "Search", { bg = "#9b8d7f", fg = "#1e1e1e" })
-	--
-	--				vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#888888", bg = "#1e1e1e" })
-	--				vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#888888", bg = "#1e1e1e" })
-	--				vim.api.nvim_set_hl(0, "LineNr", { fg = "#d6d2c8" })
-	--			end
-	--
-	--			-- Call the overrides
-	--			black_metal_theme_overrides()
-	--		end,
-	--	},
-	--	{
-	--		"frenzyexists/aquarium-vim",
-	--		config = function()
-	--			vim.cmd([[colorscheme aquarium]])
-	--			vim.cmd([[highlight NonText guibg=NONE]])
-	--		end,
-	--	},
-	--	{
-	--		"https://github.com/RRethy/base16-nvim",
-	--		config = function()
-	--			local bg = "#0F1919"
-	--			local accent = "#102121"
-	--			local accent2 = "#0D2525" -- highlight
-	--
-	--			local text = "#abb2bf"
-	--			local dark_text = "#3E4451" -- comments, line numbers
-	--
-	--			local keyword = "#8F939A"
-	--			local func = "#B6AB8B"
-	--			local types = "#65838E"
-	--			local constant = "#A06057"
-	--
-	--			local for_tesing = "#FF0000"
-	--
-	--			require("base16-colorscheme").setup({
-	--				base00 = bg,
-	--				base01 = accent,
-	--				base02 = accent2,
-	--				base03 = dark_text,
-	--				base04 = dark_text,
-	--				base05 = text,
-	--				base06 = for_tesing,
-	--				base07 = for_tesing,
-	--				base08 = text,
-	--				base09 = constant,
-	--				base0A = types,
-	--				base0B = constant,
-	--				base0C = text,
-	--				base0D = func,
-	--				base0E = keyword,
-	--				base0F = text,
-	--			})
-	--			-- vim.cmd("colorscheme base16")
-	--		end,
-	--	},
-	--	{
-	--		"sainnhe/gruvbox-material",
-	--		lazy = false,
-	--		priority = 1000,
-	--		config = function()
-	--			-- Optionally configure and load the colorscheme
-	--			-- directly inside the plugin declaration.
-	--			vim.g.gruvbox_material_enable_italic = true
-	--			vim.g.gruvbox_material_foreground = "mix"
-	--			vim.cmd.colorscheme("gruvbox-material")
-	--		end,
-	--	},
-	--	{
-	--		"2nthony/vitesse.nvim",
-	--		dependencies = {
-	--			"tjdevries/colorbuddy.nvim",
-	--			config = function()
-	--				vim.cmd("colorscheme vitesse")
-	--			end,
-	--		},
-	--	},
-	--	{
-	--		"cocopon/iceberg.vim",
-	--		config = function()
-	--			vim.cmd("colorscheme iceberg")
-	--		end,
-	--	},
-	--	{
-	--		"zenbones-theme/zenbones.nvim",
-	--		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
-	--		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-	--		-- In Vim, compat mode is turned on as Lush only works in Neovim.
-	--		dependencies = "rktjmp/lush.nvim",
-	--		lazy = false,
-	--		priority = 1000,
-	--		-- you can set set configuration options here
-	--		config = function()
-	--			vim.g.zenbones_darken_comments = 45
-	--			vim.cmd.colorscheme("zenbones")
-	--		end,
-	--	},
-	-- Lazy
-	--	{
-	--		"Verf/deepwhite.nvim",
-	--		lazy = false,
-	--		priority = 1000,
-	--		config = function()
-	--			vim.cmd([[colorscheme deepwhite]])
-	--		end,
-	--	},
+	"vague2k/vague.nvim",
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+	"dgox16/oldworld.nvim",
+	"rebelot/kanagawa.nvim",
+	"arcticicestudio/nord-vim",
+	"navarasu/onedark.nvim",
+	"rose-pine/neovim",
+	"cpwrs/americano.nvim",
+	"yorumicolors/yorumi.nvim",
+	"aktersnurra/no-clown-fiesta.nvim",
+	"mhartington/oceanic-next",
+	"metalelf0/base16-black-metal-scheme",
+	"frenzyexists/aquarium-vim",
+	"https://github.com/RRethy/base16-nvim",
+	"sainnhe/gruvbox-material",
+	"2nthony/vitesse.nvim",
+	"cocopon/iceberg.vim",
+	"zenbones-theme/zenbones.nvim",
+	"Verf/deepwhite.nvim",
 }
