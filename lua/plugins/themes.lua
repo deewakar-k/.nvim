ca70c7a6df0e8161cb7bd29522e32c2aaf23635d
deewakar-k <dakshftw01@gmail.com>
2025-04-11 19:52:26 +0000

    lazy vim

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "tjdevries/colorbuddy.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("vague")
+    end,
+  },
+  "vague2k/vague.nvim",
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+  },
+  "dgox16/oldworld.nvim",
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg = "#0a0c0f",
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+    },
+  },
+  "arcticicestudio/nord-vim",
+  "navarasu/onedark.nvim",
+  "rose-pine/neovim",
+  "cpwrs/americano.nvim",
+  "yorumicolors/yorumi.nvim",
+  "aktersnurra/no-clown-fiesta.nvim",
+  "mhartington/oceanic-next",
+  "metalelf0/base16-black-metal-scheme",
+  "frenzyexists/aquarium-vim",
+  "https://github.com/RRethy/base16-nvim",
+  "sainnhe/gruvbox-material",
+  "2nthony/vitesse.nvim",
+  "cocopon/iceberg.vim",
+  "zenbones-theme/zenbones.nvim",
+  "Verf/deepwhite.nvim",
+}
