6e7434ce10c7b33cdc1f69ffc1a6d0c29f57f5b9
deewakar-k <dakshftw01@gmail.com>
2025-04-15 13:58:18 +0530

    alpha

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("kanagawa")
+      vim.cmd.colorscheme("catppuccin-mocha")
       --vim.cmd("colorscheme base16-black-metal-gorgoroth")
     end,
   },
